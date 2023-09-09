e4c88aacedc9a786b8f2f18aee79b7349401aa67
Debjyoti Shit <debjyotishit2004@gmail.com>
2023-09-09 15:35:53 +0530

    Tower Of Hanoi

---
+++ src/Tower_Of_Hanoi.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Tower_Of_Hanoi {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter Your Number : ");
+        int number=input.nextInt();
+        System.out.println("Your "+number+" digit Tower of hanoi game step are there : ");
+        function(number,'A','B','C');
+    }
+    public static void function(int number,char src,char aux,char des){
+        if(number==1){
+            System.out.println(src+" --> "+des);
+            return;
+        }
+        function(number-1,src,des,aux);
+        // System.out.println(src+" --> "+des);  // we can use this one
+        function(1,src,aux,des);    // also this one
+        function(number-1,aux,src,des);
+    }
+
+}
+
+
